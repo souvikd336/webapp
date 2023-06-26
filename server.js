ec569a20601ce1eac89c94108372e6efcd7484e3
Souvik <souvikd336@gmail.com>
2023-06-26 12:12:35 +0530

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Cluster31317:Vl9Qfk51eVp8@cluster31317.vkfibit.mongodb.net/?retryWrites=true&w=majority";
 
-const mongoURI = "mongodb://localhost/role_auth";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-const connectToMongo = async () => {
- mongoose.connect(mongoURI, await console.log("Connected to mongo Successful")
-    );
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
-
-connectToMongo();
+run().catch(console.dir);
